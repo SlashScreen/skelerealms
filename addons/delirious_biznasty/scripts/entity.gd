945e552e381df0d92f7c9efac51a014076cf8623
Slashscreen <SlashScreen@users.noreply.github.com>
2023-03-22 03:26:01 -0700

    I need to go to bed now. Added solution file, which Ishould probably untrack.

--- addons/delirious_biznasty/scripts/entity.gd
+++ addons/delirious_biznasty/scripts/entity.gd
@@ -5,8 +5,10 @@ extends Node
 
 ## The world this entity is in.
 @export var world: String
+
 ## Position within the world it's in.
 @export var position:Vector3
+
 ## Whether this entity is in the scene or not.
 var in_scene: bool: 
 	get:
@@ -27,19 +29,10 @@ signal entered_scene
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	should_be_in_scene()
-	
-func _set_in_scene(val: bool):
-	if in_scene && !val: # if was in scene and now not
-		left_scene.emit()
-	if !in_scene && val: # if was not in scene and now is
-		entered_scene.emit()
-	in_scene = val
-	
-func _get_in_scene() -> bool:
-	return in_scene
+	_should_be_in_scene()
 
-func should_be_in_scene():
+
+func _should_be_in_scene():
 	# if not in correct world
 	if %GameInfo.world != world:
 		in_scene = false
@@ -50,13 +43,20 @@ func should_be_in_scene():
 		return
 	in_scene = true
 
+
 func _on_set_position(p:Vector3):
 	position = p
 
+
+## Gets a component by the string name. 
+## Example: [codeblock]
+## (e.get_component("NPCComponent").unwrap() as NPCComponent).kill()
 func get_component(type:String) -> Option:
 	var n = get_node_or_null(type)
 	return Option.from(n)
 
+
+## Whether it has a component type or not. Useful for checking the capabilities of an entity.
 func has_component(type:String) -> bool:
 	var x = get_component(type)
 	return x.some()
