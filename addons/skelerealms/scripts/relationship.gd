ac39a3a6393a5cb0f0f864ac268736f117728b21
Slashscreen <SlashScreen@users.noreply.github.com>
2023-04-06 22:57:58 -0700

    added relationship level. Test push to new remote.

--- addons/skelerealms/scripts/relationship.gd
+++ addons/skelerealms/scripts/relationship.gd
@@ -3,6 +3,21 @@ extends Resource
 
 
 @export var other_person:String
+@export var level:RelationshipLevel = RelationshipLevel.ACQUAINTANCE
 @export_category("Optional")
 @export var relationship_type:RelationshipAssociation
 @export var role:String # gotta figure out how to make it dynamic enum
+
+
+## Level determining how close the two NPCs are.
+enum RelationshipLevel {
+	NEMESIS, ## NPC Will always engage on sight.
+	ENEMY, ## Depending on combat settings, NPC may engage this level and below on sight.
+	FOE, ## Dislike eachother a lot.
+	RIVAL, ## Homestuck tells me these also smooch.
+	ACQUAINTANCE, ## No real relationship.
+	FRIEND, ## Friendly.
+	BFF, ## Closer friend.
+	ALLY, ## Depending on NPC behavior settings, may assist this level and above in combat.
+	LOVER, ## smooch smooch smooch
+}
