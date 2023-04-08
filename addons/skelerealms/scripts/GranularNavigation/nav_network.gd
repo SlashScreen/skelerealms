82712ad03605598889b64c9cd4c27be781ecce5a
Slashscreen <SlashScreen@users.noreply.github.com>
2023-04-08 00:10:16 -0700

    nav network part 2: this code is turning into a disaster. every game is made of duct tape. need to rewrite

--- addons/skelerealms/scripts/GranularNavigation/nav_network.gd
+++ addons/skelerealms/scripts/GranularNavigation/nav_network.gd
@@ -1,8 +1,16 @@
 class_name NavNetwork
 extends Resource
+## A granular navigation network that allows agents to navigate outside of currently loaded scenes. 
+## These are auto-converted to the K-D tree system on runtime, which is dumb.
+## This whole thing is scuffed and may warrant a rewrite of some kind to be more efficient, but I need it to work 
+## more than I need it to be efficient right now. 
+## Could improve by having it turn itself into a K-D tree at build time, not runtime. 
+#! This wretched thing could be a performance pain point.
 
 
+## A single point in the network.
 class NetPoint:
+	extends Resource
 	var point:Vector3
 	var connections:Dictionary = {} # node : cost
 
@@ -16,7 +24,18 @@ class NetPoint:
 		point = pt
 
 
-var _points:Array[NavPoint] = []
+## This portal allows you to link this network to other [NavNetwork]s.
+class NetPortal: # no earthly idea how im going to make this intuitive
+	extends NetPoint
+
+	@export var id:StringName
+	@export var other_side:NetPortal
+	@export var world:StringName
+
+
+@export var portals:Array[NetPortal] = []
+@export var world:StringName
+var _points:Array[NetPoint] = []
 
 
 # TODO: Allow for cross-world conenctions. Add portal object?
