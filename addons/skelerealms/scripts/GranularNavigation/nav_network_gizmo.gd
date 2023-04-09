341b799e2ba329fa9e9c19749cde6cf2c46b30dc
Slashscreen <SlashScreen@users.noreply.github.com>
2023-04-08 22:30:28 -0700

    ok dragging around stuff works now, but not adding anything

--- addons/skelerealms/scripts/GranularNavigation/nav_network_gizmo.gd
+++ addons/skelerealms/scripts/GranularNavigation/nav_network_gizmo.gd
@@ -1,6 +1,10 @@
 class_name NavNetworkGizmo
 extends EditorNode3DGizmoPlugin
 
+const RAY_LENGTH = 500
+
+var associations:Array[NetPoint] = []
+
 
 func _init() -> void:
 	create_handle_material("handles")
@@ -13,9 +17,15 @@ func _has_gizmo(for_node_3d: Node3D) -> bool:
 
 func _redraw(gizmo: EditorNode3DGizmo) -> void:
 	gizmo.clear()
+	associations.clear()
+
 	var handle_points = PackedVector3Array()
+	var i = 0
 
 	var net = gizmo.get_node_3d() as NavigationNetwork3D
+	associations.resize(net.network._points.size())
+	var handle_idx:Array = [] # styupid but it should work now
+
 	# Aggeregate lines?
 	for pt in net.network._points:
 		handle_points.append(pt.point)
@@ -27,14 +37,32 @@ func _redraw(gizmo: EditorNode3DGizmo) -> void:
 		gizmo.add_mesh(mesh, get_material("gizmo_mat", gizmo), t)
 		gizmo.add_collision_triangles(mesh)
 		# draw lines between all connections
+		associations[i] = pt
+		handle_idx.append(i)
 		# TODO: Recurse
 		for c in pt.connections:
 			var pts = PackedVector3Array()
 			pts.append(pt.point)
 			pts.append(c.point)
 			gizmo.add_lines(pts, get_material("gizmo_mat", gizmo))
+		
+		i += 1
 	
-	gizmo.add_handles(handle_points, get_material("handles", gizmo), [])
+	gizmo.add_handles(handle_points, get_material("handles", gizmo), handle_idx)
+
+
+func _set_handle(gizmo: EditorNode3DGizmo, handle_id: int, secondary: bool, camera: Camera3D, screen_pos: Vector2) -> void:
+	var pt = associations[handle_id]
+	var from = camera.project_ray_origin(screen_pos)
+	var to = from + (camera.project_ray_normal(screen_pos) * RAY_LENGTH)
+	var ray = PhysicsRayQueryParameters3D.create(from, to)
+	await gizmo.get_node_3d().get_tree().physics_frame
+	var hits = gizmo.get_node_3d().get_world_3d().direct_space_state.intersect_ray(ray)
+	#print(handle_id)
+	#print(hits)
+	if hits:
+		pt.point = hits["position"] as Vector3
+		_redraw(gizmo)
 
 
 func _get_gizmo_name() -> String:
