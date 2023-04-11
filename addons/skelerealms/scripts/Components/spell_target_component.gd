7dc88dba9ed3ef123301fc34d55913edcb005afb
Slashscreen <SlashScreen@users.noreply.github.com>
2023-04-10 20:38:02 -0700

    Added raycasting to spell

--- addons/skelerealms/scripts/Components/spell_target_component.gd
+++ addons/skelerealms/scripts/Components/spell_target_component.gd
@@ -1,11 +1,18 @@
 class_name SpellTargetComponent
 extends EntityComponent
+## Allows entities to be hit with spells, and keeps track of any applied spell effects.
 
 
 var active_effects:Array[SpellEffect] = []
 
 
 signal hit_with_spell(spell:Spell)
+signal added_effect(effect:SpellEffect)
+
+
+## Hit this entity with a spell. Doesn't actually do anything apart from emit [signal hit_with_spell]. To apply effects, you can do that on the [Spell] side.
+func hit(spell:Spell) -> void:
+	hit_with_spell.emit(spell)
 
 
 func _init() -> void:
@@ -18,11 +25,13 @@ func _process(delta: float) -> void:
 		eff.on_update(delta)
 
 
-func add_effect(effect:SpellEffect) -> void:
+## Duplicates an initializes a new spell effect. Returns the new effect.
+func add_effect(effect:SpellEffect) -> SpellEffect:
 	var eff = effect.duplicate(true)
 	eff.apply(self) # bind to this
 	active_effects.append(eff)
 	eff.on_start_effect() # callback
+	return eff
 
 
 ## Dispel an effect.
--- addons/skelerealms/scripts/SpellCasting/spell.gd
+++ addons/skelerealms/scripts/SpellCasting/spell.gd
@@ -1,6 +1,6 @@
 class_name Spell
 extends Resource
-## This the base class for any spell that the player can cast.
+## This the base class for any spell that NPCs can cast.
 ## This essentially a blank slate, and as this is a GDScript file, a spell can do literally anything, sky is the limit.
 ## However, with great control comes great responsibility, my uncle once told me. This means you need to deal with basic stuff, like willpower drain, yourself.
 ## Despite that, it includes a number of helper methods to do the simple stuff. See [method _find_spell_targets_in_range].
@@ -8,9 +8,11 @@ extends Resource
 
 ## Spell's ID for translation.
 @export var spell_name:String
-## Custom assets for this spell; particles, floating hand models, spell effects, whatever.
+## Custom assets for this spell; particles, floating hand models, whatever.
 ## You can pack it all in here so you don't have to use load(), but you can still do that if you want to.
 @export var spell_assets:Dictionary
+## An array of spell effects we can apply to something we hit.
+@export var spell_effects:Array[SpellEffect]
 ## Whether being hit by this spell is to be considered an attack.
 @export var aggressive:bool = false
 ## The spell caster.
@@ -40,11 +42,11 @@ func reset():
 	pass
 
 
-## Find all nodes in a range of a point.
+## Find all nodes in a range of a point, with the results in a dictionary matching a physics query (like raycasting). 
 ## You can use this for an AOE attack.
-## using ignore_self assumes that the caster's origin defines the root of the actor casting it.
+## Using ignore_self assumes that the caster's origin defines the root of the actor casting it.
 ## Only returns 32 results max.
-func _find_spell_targets_in_range(pos:Vector3, radius:float, ignore_self:bool = false) -> Array[Node]:
+func _find_spell_targets_in_range(pos:Vector3, radius:float, ignore_self:bool = false) -> Dictionary:
 	var space_state = _caster.get_world_3d().direct_space_state # get space state
 	# create query
 	var query = PhysicsShapeQueryParameters3D.new()
@@ -55,6 +57,7 @@ func _find_spell_targets_in_range(pos:Vector3, radius:float, ignore_self:bool = 
 	t.scaled(Vector3(radius, radius, radius)) # scale to match radius
 	query.transform = t
 	# make query
+	await _caster.get_tree().physics_frame
 	var res = space_state.intersect_shape(query)
 	# if ignoring self, filter out all nodes part of this tree
 	if ignore_self:
@@ -62,7 +65,7 @@ func _find_spell_targets_in_range(pos:Vector3, radius:float, ignore_self:bool = 
 			return not (x["collider"] as Node).is_ancestor_of(_caster) and not (x["collider"] as Node).find_child(_caster.name)
 		)
 	# return results, where all colliders are selected from it.
-	return res.map(func(x:Dictionary): return x["collider"] as Node)
+	return res
 
 
 ## Apply a spell effect to an object.
@@ -71,8 +74,19 @@ func _apply_spell_effect_to(target, effect:SpellEffect):
 	# return early if invalid object
 	if not target is SpellTargetComponent or not target is SpellTargetObject:
 		return
-	target.apply_effect(effect)
-# TODO:
-# 	raycast
-# 	spawn particle, node
-# 	apply spell effect
+	target.add_effect(effect)
+
+
+## Casts a ray, and returns anything it hits, with the results in a dictionary matching a physics query (like raycasting). The dictionary is empty if it hits nothing.
+## Using ignore_self assumes that the caster's origin defines the root of the actor casting it.
+func _raycast(from:Vector3, direction:Vector3, distance:float, ignore_self:bool = false) -> Dictionary:
+	var to = from + (direction * distance)
+	var ray = PhysicsRayQueryParameters3D.create(from, to)
+	var space_state = _caster.get_world_3d().direct_space_state # get space state
+	await _caster.get_tree().physics_frame
+	var res = space_state.intersect_ray(ray)
+	if ignore_self:
+		return res.filter(func(x:Dictionary):
+			return not (x["collider"] as Node).is_ancestor_of(_caster) and not (x["collider"] as Node).find_child(_caster.name)
+		)
+	return res
