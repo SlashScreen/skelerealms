044e4182a6a1716e1502daad682282e35a5973fe
Slashscreen <SlashScreen@users.noreply.github.com>
2023-04-11 23:19:00 -0700

    Made audio event emitter a sub node

--- addons/skelerealms/scripts/audio_emitter.gd
+++ addons/skelerealms/scripts/audio_emitter.gd
@@ -1,15 +1,15 @@
 class_name AudioEventEmitter
-extends AudioStreamPlayer3D # TODO: Allow any backend
+extends Node3D
 ## Used to emit sounds that should have an effect on other things, like alerting NPCs.
+## Put this beneath some sort of audio emitter, and link signals.
 
 
 @export var ignore_self:bool = false
 
 
 ## Finds every node of group "audio_listener" in a radius of unit_size using a physics shape cast, and attempts to call method "heard_audio" on it, passing self as an argument.
-func play_event():
+func send_play_event(range:float):
 	# TODO: Make it less physics based?
-	play()
 	var space_state = get_world_3d().direct_space_state # get space state
 	# create query
 	var query = PhysicsShapeQueryParameters3D.new()
@@ -17,7 +17,7 @@ func play_event():
 	# create query position
 	var t = Transform3D()
 	t.origin = position
-	t.scaled(Vector3(unit_size, unit_size, unit_size)) # scale to match radius
+	t.scaled(Vector3(range, range, range)) # scale to match radius
 	query.transform = t
 	# make query
 	var res = space_state.intersect_shape(query)
