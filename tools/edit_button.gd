9862ad258ea6e15ace4de5a48bdae12bcfe8d6f1
Slashscreen <SlashScreen@users.noreply.github.com>
2024-09-06 16:45:18 -0700

    working on netwrk-based backend

--- tools/edit_button.gd
+++ tools/edit_button.gd
@@ -2,18 +2,62 @@
 extends Button
 
 
-const EditorGameRoot = preload("res://addons/skelerealms/tools/editor_game_root.gd")
+var sock:ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+var spawn_position:Vector3
+var spawn_world:String
 
 
 func _ready() -> void: 
 	text = "Open Skelerealms World"
 	pressed.connect(_on_press.bind())
+	_open_sock()
 
 
 func _on_press() -> void:
 	var cam:Camera3D = EditorInterface.get_editor_viewport_3d(0).get_camera_3d()
-	EditorGameRoot.spawn_position = cam.position
-	EditorGameRoot.spawn_world = EditorInterface.get_edited_scene_root().name
+	spawn_position = cam.position
+	spawn_world = EditorInterface.get_edited_scene_root().name
 	
 	var game_root_path:String = SkeleRealmsGlobal.config.game_root.resource_path
 	EditorInterface.play_custom_scene(game_root_path)
+
+
+func _serve() -> void:
+	sock.poll()
+	var state := sock.get_connection_status()
+	if state == WebSocketPeer.STATE_OPEN:
+		while sock.get_available_packet_count():
+			if sock.get_packet() == PackedByteArray([1, 2, 3, 2, 1]):
+				_push_spawn_data()
+	elif state == WebSocketPeer.STATE_CLOSING:
+		# Keep polling to achieve proper close.
+		pass
+	elif state == WebSocketPeer.STATE_CLOSED:
+		var code = sock.get_close_code()
+		var reason = sock.get_close_reason()
+		#print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+
+
+func _open_sock() -> void:
+	if not sock.create_server(8123) == OK:
+		push_warning("Could not start Skelerealms play button service.")
+	if sock.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		push_warning("Could not start Skelerealms play button service.")
+		return
+	sock.peer_connected.connect(func(id:int) -> void: 
+		print("Connected to peer: %s" % id)
+		_push_spawn_data()
+		)
+	sock.peer_disconnected.connect(func(id:int) -> void: print("Peer disconnected: %d" % id))
+
+
+func _push_spawn_data() -> void:
+	var data = {
+		"pos":spawn_position,
+		"world":spawn_world,
+	}
+	sock.put_packet(JSON.stringify(data).to_ascii_buffer())
+
+
+func _process(delta):
+	_serve()
--- tools/editor_game_root.gd
+++ tools/editor_game_root.gd
@@ -1,15 +1,55 @@
-@tool
 extends Node3D
 
 
 static var spawn_position:Vector3
 static var spawn_world:String
 
+var sock:ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+var sent:bool
 
-func spawn_default() -> void:
-	if spawn_world.is_empty():
-		spawn_world = SkeleRealmsGlobal.config.default_world
+
+func _client_ready() -> void:
+	_open_sock()
+
+
+func _open_sock() -> void:
+	if not sock.create_client("localhost", 8123) == OK:
+		push_warning("Could not connect to Skelerealms play button.")
+
+
+func _push_spawn_data() -> void:
+	var data = {
+		"pos":spawn_position,
+		"world":spawn_world,
+	}
 	
-	# TODO: Get entity manager by type
-	var tc:TeleportComponent = ($EntityManager as SKEntityManager).get_entity(&"Player").get_component(&"TeleportComponent")
-	tc.teleport(spawn_world, spawn_position)
+	sock.send_text(JSON.stringify(data))
+
+
+func _process(delta):
+	_listen()
+
+
+func _ask_for_data() -> void:
+	sock.put_packet(PackedByteArray([1, 2, 3, 2, 1]))
+
+
+func _listen() -> void:
+	sock.poll()
+	var state = sock.get_connection_status()
+	if state == WebSocketPeer.STATE_OPEN:
+		while sock.get_available_packet_count():
+			_move_player(JSON.parse_string(sock.get_packet().get_string_from_utf8()))
+			sock.close()
+	elif state == WebSocketPeer.STATE_CLOSING:
+		# Keep polling to achieve proper close.
+		pass
+	elif state == WebSocketPeer.STATE_CLOSED:
+		var code = sock.get_close_code()
+		var reason = sock.get_close_reason()
+		print("WebSocket closed with code: %d, reason %s. Clean: %s" % [code, reason, code != -1])
+		set_process(false)
+
+
+func _move_player(data:Dictionary) -> void:
+	print("Got data: ", data)
