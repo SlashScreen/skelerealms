293dfd0b68541066bc505eadf03255d85218b4bf
Slashscreen <SlashScreen@users.noreply.github.com>
2024-09-15 11:48:29 -0700

    bump version. oops.

--- tools/edit_button.gd
+++ tools/edit_button.gd
@@ -17,11 +17,7 @@ func _ready() -> void:
 
 func _on_press() -> void:
 	var cam:Camera3D = EditorInterface.get_editor_viewport_3d(0).get_camera_3d()
-	#spawn_position = cam.position
-	#spawn_world = EditorInterface.get_edited_scene_root().name
-	
-	
-	#_open_sock()
+
 	OS.set_environment("world", EditorInterface.get_edited_scene_root().name)
 	OS.set_environment("pos", var_to_str(cam.position))
 	
@@ -29,40 +25,3 @@ func _on_press() -> void:
 	EditorInterface.play_custom_scene(game_root_path)
 	OS.set_environment("world", "")
 	OS.set_environment("pos", "")
-
-
-func _serve() -> void:
-	sock.poll()
-	var state := sock.get_connection_status()
-	if state == MultiplayerPeer.CONNECTION_CONNECTED:
-		while sock.get_available_packet_count():
-			if sock.get_packet() == PackedByteArray([1, 2, 3, 2, 1]):
-				_push_spawn_data()
-				sock.close()
-				sending = false
-
-
-func _open_sock() -> void:
-	if not sock.create_server(8123) == OK:
-		push_warning("Could not start Skelerealms play button service.")
-	if sock.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
-		push_warning("Could not start Skelerealms play button service.")
-		return
-	sock.peer_connected.connect(func(id:int) -> void: 
-		_push_spawn_data()
-		)
-	sending = true
-
-
-func _push_spawn_data() -> void:
-	var data = {
-		"pos":spawn_position,
-		"world":spawn_world,
-	}
-	if not sock.put_packet(var_to_bytes(data)) == OK:
-		push_warning("Failed to send Skelerealms play button data.")
-
-
-func _process(_delta) -> void:
-	if sending:
-		_serve()
